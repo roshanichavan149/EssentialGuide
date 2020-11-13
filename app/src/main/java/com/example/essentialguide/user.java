9f5e5c5eb08b0948aab8b93f7d15f324e75b0dc1
Roshani Chavan <73379995+roshanichavan149@users.noreply.github.com>
2020-11-13 14:32:44 +0530

    project added

---
+++ app/src/main/java/com/example/essentialguide/user.java
@@ -0,0 +1,32 @@
+package com.example.essentialguide;
+
+public class user {
+    String shopName,ShopAddress,shopContact;
+
+    //empty constructor
+    public user(){}
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getShopAddress() {
+        return ShopAddress;
+    }
+
+    public void setShopAddress(String shopAddress) {
+        ShopAddress = shopAddress;
+    }
+
+    public String getShopContact() {
+        return shopContact;
+    }
+
+    public void setShopContact(String shopContact) {
+        this.shopContact = shopContact;
+    }
+}
